fadde4d185bc9b50dcb7000c3a5da124f6594818
David Makuni <david.makuni@cytora.com>
2020-11-18 20:12:19 +0000

    initial files

---
+++ core/Footer.js
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require("react");
+
+class Footer extends React.Component {
+  docUrl(doc) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+    return `${baseUrl}${docsPart}${doc}`;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <div>
+            <h5>Topics</h5>
+            <a href={this.docUrl("quickstart")}>Quickstart</a>
+            <a href={this.docUrl("overview")}>Overview</a>
+            <a href={this.docUrl("philosophy")}>Philosophy</a>
+            <a href={this.docUrl("security")}>Security</a>
+            <a href={this.docUrl("terminology")}>Terminology</a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://github.com/daviemakz/risen-js"
+            >
+              Code Repository
+            </a>
+            <a
+              href="https://github.com/daviemakz/risen-js/issues"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Issues
+            </a>
+            <a
+              target="_blank"
+              href={`https://github.com/daviemakz/risen-js/pulls`}
+            >
+              Pull Requests
+            </a>
+
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://stackoverflow.com/search?q=risen-js"
+            >
+              Stack Overflow
+            </a>
+          </div>
+          <div>
+            <h5>Links</h5>
+
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://www.npmjs.com/package/risen-js"
+            >
+              NPM
+            </a>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://travis-ci.org/daviemakz/risen-js"
+            >
+              Build Status
+            </a>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://app.fossa.io/projects/git%2Bgithub.com%2Fdaviemakz%2Frisen-js/"
+            >
+              Licence Status
+            </a>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://libraries.io/github/daviemakz/risen-js"
+            >
+              Dependencies
+            </a>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://github.com/daviemakz/risen-js"
+            >
+              <img
+                alt="GitHub Repo stars"
+                src="https://img.shields.io/github/stars/daviemakz/risen-js?style=social"
+              />
+            </a>
+
+            {this.props.config.twitterUsername && (
+              <div className="social">
+                <a
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
+                  className="twitter-follow-button"
+                >
+                  Follow @{this.props.config.twitterUsername}
+                </a>
+              </div>
+            )}
+            {this.props.config.facebookAppId && (
+              <div className="social">
+                <div
+                  className="fb-like"
+                  data-href={this.props.config.url}
+                  data-colorscheme="dark"
+                  data-layout="standard"
+                  data-share="true"
+                  data-width="225"
+                  data-show-faces="false"
+                />
+              </div>
+            )}
+          </div>
+        </section>
+        <a
+          href="https://opensource.facebook.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="fbOpenSource"
+        >
+          <img
+            src={`${this.props.config.baseUrl}img/oss_logo.png`}
+            alt="Facebook Open Source"
+            width="170"
+            height="45"
+          />
+        </a>
+        <section className="copyright">{this.props.config.copyright}</section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
---
+++ pages/en/help.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require("react");
+
+const CompLibrary = require("../../core/CompLibrary.js");
+
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+function Help(props) {
+  const { config: siteConfig, language = "" } = props;
+  const { baseUrl, docsUrl } = siteConfig;
+  const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+  const langPart = `${language ? `${language}/` : ""}`;
+  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+  const supportLinks = [
+    {
+      content: `Learn more about Risen.JS by viewing the [documentation on this site.](${docUrl(
+        "quickstart"
+      )})`,
+      title: "Browse Docs"
+    },
+    {
+      content:
+        "If you need help or are having trouble please create an issue by following this [link](https://github.com/daviemakz/risen-js/issues/new/choose).",
+      title: "Create an issue"
+    },
+    {
+      content:
+        "If you want to have a look at the codebase please visit this [link](https://github.com/daviemakz/risen-js).",
+      title: "Check out the code"
+    }
+  ];
+
+  return (
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer documentContainer postContainer">
+        <div className="post">
+          <img
+            alt="Designed by vectorjuice / Freepik"
+            src={`${baseUrl}img/help.jpg`}
+            style={{
+              width: "40%",
+              margin: "0 auto",
+              display: "block"
+            }}
+          />
+          <header className="postHeader">
+            <h1>Need help?</h1>
+          </header>
+          <p>
+            Have you bumped into an issue? Cant get the framework to start? You
+            think you've bumped into a bug. Check out the sections below for
+            more help.
+          </p>
+          <GridBlock contents={supportLinks} layout="threeColumn" />
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+module.exports = Help;
